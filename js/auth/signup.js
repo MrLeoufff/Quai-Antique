74f93c63f440efada3fd1261aaedb4db237184db
mrleoufff <mrleoufff@gmail.com>
2024-07-27 19:45:04 +0200

    Validation mail, password

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -17,8 +17,9 @@ function validateForm() {
     const nomOk = validateRequired(inputNom,);
     const prenomOk = validateRequired(inputPrenom,);
     const mailOk = validateMail(inputMail);
+    const passwordOk = validatePassword(inputPassword);
 
-    if (nomOk && prenomOk && mailOk) {
+    if (nomOk && prenomOk && mailOk && passwordOk) {
         btnValidation.disabled = false;
     } else {
         btnValidation.disabled = true;
@@ -41,6 +42,20 @@ function validateMail(input) {
     const emaiRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const mailUser = input.value;
     if (mailUser.match(emaiRegex)) {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if (passwordUser.match(passwordRegex)) {
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
         return true;
