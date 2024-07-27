6c12ce49e93b9c372bec7d1ac541d6271d283b6e
mrleoufff <mrleoufff@gmail.com>
2024-07-27 19:13:54 +0200

    reservation

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -5,6 +5,7 @@ const inputPrenom = document.getElementById("PrenomInput");
 const inputMail = document.getElementById("EmailInput");
 const inputPassword = document.getElementById("PasswordInput");
 const inputValidatePassword = document.getElementById("ValidatePasswordInput");
+const btnValidation = document.getElementById("btn-validation-inscription");
 
 inputNom.addEventListener("keyup", validateForm);
 inputPrenom.addEventListener("keyup", validateForm);
@@ -13,16 +14,39 @@ inputPassword.addEventListener("keyup", validateForm);
 inputValidatePassword.addEventListener("keyup", validateForm);
 
 function validateForm() {
-    validateRequired(inputNom,);
-    validateRequired(inputPrenom,);
+    const nomOk = validateRequired(inputNom,);
+    const prenomOk = validateRequired(inputPrenom,);
+    const mailOk = validateMail(inputMail);
+
+    if (nomOk && prenomOk && mailOk) {
+        btnValidation.disabled = false;
+    } else {
+        btnValidation.disabled = true;
+    }
 }
 
 function validateRequired(input) {
     if (input.value != '') {
         input.classList.add("is-valid");
         input.classList.remove("is-invalid");
+        return true;
     } else {
         input.classList.add("is-invalid");
         input.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validateMail(input) {
+    const emaiRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const mailUser = input.value;
+    if (mailUser.match(emaiRegex)) {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+        return false;
     }
 }
