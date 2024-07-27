551f5ae26fae3b51516b5e70f5ad0341b840dd42
mrleoufff <mrleoufff@gmail.com>
2024-07-27 18:56:17 +0200

    reservation

---
+++ js/auth/signup.js
@@ -0,0 +1,28 @@
+// Implémenter le JS de ma page
+
+const inputNom = document.getElementById("NomInput");
+const inputPrenom = document.getElementById("PrenomInput");
+const inputMail = document.getElementById("EmailInput");
+const inputPassword = document.getElementById("PasswordInput");
+const inputValidatePassword = document.getElementById("ValidatePasswordInput");
+
+inputNom.addEventListener("keyup", validateForm);
+inputPrenom.addEventListener("keyup", validateForm);
+inputMail.addEventListener("keyup", validateForm);
+inputPassword.addEventListener("keyup", validateForm);
+inputValidatePassword.addEventListener("keyup", validateForm);
+
+function validateForm() {
+    validateRequired(inputNom,);
+    validateRequired(inputPrenom,);
+}
+
+function validateRequired(input) {
+    if (input.value != '') {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+    } else {
+        input.classList.add("is-invalid");
+        input.classList.remove("is-valid");
+    }
+}
